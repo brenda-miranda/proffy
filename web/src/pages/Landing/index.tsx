896e6bb6da991118713d3f788d69cbdfa9e1f0b4
Brenda Miranda <brendamndx@gmail.com>
2020-08-03 11:55:30 -0300

    Import css to landing page

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './styles.css';
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
 import studyIcon from '../../assets/images/icons/study.svg';
