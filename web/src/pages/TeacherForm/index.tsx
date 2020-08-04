88960b961311a869a656ce3ddd7fc8acfc8419dd
Brenda Miranda <brendamndx@gmail.com>
2020-08-04 12:03:22 -0300

    Add props to PageHeader in TeacherForm

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -4,7 +4,7 @@ import PageHeader from '../../components/PageHeader';
 function TeacherForm() {
    return (
       <div id="page-teacher-form" className="container">
-         <PageHeader />
+         <PageHeader title="Que incrível que você quer dar aulas " />
       </div>
    )
 }
