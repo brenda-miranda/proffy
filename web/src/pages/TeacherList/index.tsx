936d020c93d1441d7ac86699eb1af8b5237011d6
Brenda Miranda <brendamndx@gmail.com>
2020-08-04 12:46:26 -0300

    Create TeacherList form

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -5,7 +5,24 @@ import './styles.css';
 function TeacherList() {
    return (
       <div id="page-teacher-list" className="container">
-         <PageHeader title="Estes são os proffys disponíveis: " />
+         <PageHeader title="Estes são os proffys disponíveis: ">
+            <form id="search-teachers">
+               <div className="input-block">
+                  <label htmlFor="subject">Matéria</label>
+                  <input type="text" id="subject" />
+               </div>
+
+               <div className="input-block">
+                  <label htmlFor="week_day">Dia da semana</label>
+                  <input type="text" id="week_day" />
+               </div>
+
+               <div className="input-block">
+                  <label htmlFor="time">Horário</label>
+                  <input type="text" id="time" />
+               </div>
+            </form>
+         </PageHeader>
       </div>
    )
 }
