22ad89bc9bd791063a5922938b5bae32ed33abd2
Brenda Miranda <brendamndx@gmail.com>
2020-08-04 12:46:55 -0300

    Import styles to PageHeader

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import logoImg from '../../assets/images/logo.svg';
 import backIcon from '../../assets/images/icons/back.svg';
+import './styles.css';
 
 interface PageHeaderProps {
    title: string;
@@ -19,6 +20,8 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
          
          <div className="header-content">
             <strong>{props.title}</strong>
+
+            {props.children}
          </div>
       </header>
    );
