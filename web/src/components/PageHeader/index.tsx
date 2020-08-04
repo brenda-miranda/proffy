8c00f71d16e8758bdab8dd0aa2a0e90ad09bf746
Brenda Miranda <brendamndx@gmail.com>
2020-08-04 12:03:50 -0300

    Turn function component into a const component and add props

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -3,7 +3,11 @@ import { Link } from 'react-router-dom';
 import logoImg from '../../assets/images/logo.svg';
 import backIcon from '../../assets/images/icons/back.svg';
 
-function PageHeader() {
+interface PageHeaderProps {
+   title: string;
+}
+
+const PageHeader: React.FC<PageHeaderProps> = (props) => {
    return (
       <header className="page-header">
             <div className="top-bar-container">
@@ -14,7 +18,7 @@ function PageHeader() {
             </div>
          
          <div className="header-content">
-            <strong>Estes são os proffys disponíveis:</strong>
+            <strong>{props.title}</strong>
          </div>
       </header>
    );
