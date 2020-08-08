cb58d10a8ace057a3cb198061b80abc44e131bfd
Brenda Miranda <brendamndx@gmail.com>
2020-08-08 18:31:56 -0300

    Create a function to update number of total connections

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
@@ -6,8 +6,19 @@ import studyIcon from '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 import './styles.css';
+import api from '../../services/api';
 
 function Landing() {
+   const [totalConnections, setTotalConnections] = useState(0);
+
+   useEffect(() => {
+      api.get('connections').then(response => {
+         const { total } = response.data;
+
+         setTotalConnections(total);
+      })
+   }, []);
+
    return (
       <div id="page-landing">
          <div id="page-landing-content" className="container">
@@ -35,7 +46,7 @@ function Landing() {
             </div>
 
             <span className="total-connections">
-               Total de 200 conexões já realizadas
+               Total de {totalConnections} conexões já realizadas
                <img src={purpleHeartIcon} alt="Coração roxo"/>
             </span>
          </div>
